8f6026c196a61cf66fa3b7bb996da45d8e8d926f
yoriai-chaya <takashi9571@gmail.com>
2025-10-28 06:17:48 +0900

    #048 Prompt Improved (G11A)

---
+++ sample/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainNav from './MainNav';
+import MobileNav from './MobileNav';
+
+const Header = () => {
+    return (
+        <header className="sticky top-0 z-50 w-full border-b bg-white text-neutral-900">
+            <div className="flex h-14 mx-2 items-center">
+                <MainNav />
+                <MobileNav />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
